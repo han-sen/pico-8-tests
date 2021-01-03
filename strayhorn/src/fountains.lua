9c0ec9e7e2a89bb2a1606b19a6c108afc39ba69e
Michael Hansen <michaelandhansen@gmail.com>
2021-01-03 15:46:23 -0500

    added projectile system with player targeting

---
+++ strayhorn/src/fountains.lua
@@ -0,0 +1,43 @@
+fountains = {
+    anim = 0,
+    sp1 = 25,
+    sp2 = 26,
+    loc = {
+        {
+            level = 1,
+            x = 96,
+            y = 104,
+            active = true
+        },
+        {
+            level = 2,
+            x = 80,
+            y = 48,
+            active = false
+        },
+        {
+            level = 3,
+            x = 16,
+            y = 24,
+            active = false
+        },
+        {
+            level = 4,
+            x = 64,
+            y = 80,
+            active = false
+        },
+        {
+            level = 5,
+            x = 64,
+            y = 112,
+            active = false
+        },
+        {
+            level = 6,
+            x = 88,
+            y = 112,
+            active = false
+        }
+    }
+}
---
+++ strayhorn/src/game_keys.lua
@@ -0,0 +1,42 @@
+game_keys = {
+    sp = 21,
+    loc = {
+        {
+            level = 1,
+            x = 0,
+            y = 0,
+            found = true
+        },
+        {
+            level = 2,
+            x = 96,
+            y = 80,
+            found = false
+        },
+        {
+            level = 3,
+            x = 96,
+            y = 80,
+            found = false
+        },
+        {
+            level = 4,
+            x = 120,
+            y = 56,
+            found = false
+        },
+        {
+            level = 5,
+            x = 88,
+            y = 32,
+            found = false
+        },
+        {
+            level = 6,
+            x = 40,
+            y = 32,
+            found = false
+        }
+    },
+    anim = 0
+}
