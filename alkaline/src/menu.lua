40a5a1392b325bc7296ef60f7ad032050699ba41
Michael Hansen <michaelandhansen@gmail.com>
2021-02-08 20:33:07 -0500

    finished title menu/title screen

--- alkaline/src/menu.lua
+++ alkaline/src/menu.lua
@@ -3,12 +3,15 @@ function update_menu()
         -- music(3)
         state = "playing"
     end
+    fire_fx(64, 112, 8, {8, 2, 1}, 2)
+    update_fx()
 end
 
 function draw_menu()
     cls()
     map(112, 16)
+    draw_fx()
     -- rect(0, 0, 127, 127, 1) -- bg color
     -- rectfill(0, 60, 127, 80, 1) -- text bar
-    print("press x to start / restart", 12, 68, 7)
+    print("press x to start / restart", 12, 84, 7)
 end
