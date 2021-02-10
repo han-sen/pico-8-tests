82f69022755e22daaff943fe5727183de463039d
Michael Hansen <michaelandhansen@gmail.com>
2021-02-09 19:47:55 -0500

    added animations to menu screen

--- alkaline/src/menu.lua
+++ alkaline/src/menu.lua
@@ -4,6 +4,16 @@ function update_menu()
         state = "playing"
     end
     fire_fx(64, 112, 8, {8, 2, 1}, 2)
+    local menutime = 0
+    if time() - world.menu_anim_1 > 1.3 then
+        world.menu_anim_1 = time()
+        bubble_fx(32, 24, 8, particles.bubble_fx, 1)
+        bubble_fx(96, 24, 8, particles.bubble_fx, 1)
+    end
+    if time() - world.menu_anim_2 > 2.6 then
+        world.menu_anim_2 = time()
+        bubble_fx(64, 24, 8, particles.bubble_fx, 1)
+    end
     update_fx()
 end
 
